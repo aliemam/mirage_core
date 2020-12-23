5bb22f59a2a7d424bf7715108aceb79361203284
AliEm <aliemamhadi@gmail.com>
2020-12-23 10:55:47 +0200

    add beanstack interface

---
+++ src/interfaces/BeanstalkInterface.php
@@ -0,0 +1,33 @@
+<?php
+/*
+  +------------------------------------------------------------------------+
+  | Mirage Framework                                                       |
+  +------------------------------------------------------------------------+
+  | Copyright (c) 2018-2020                                                |
+  +------------------------------------------------------------------------+
+  | This source file is subject to the New BSD License that is bundled     |
+  | with this package in the file LICENSE.txt.                             |
+  |                                                                        |
+  | If you did not receive a copy of the license and are unable to         |
+  | obtain it through the world-wide-web, please send an email             |
+  | to help@aemirage.com so we can send you a copy immediately.            |
+  +------------------------------------------------------------------------+
+  | Authors: Ali Emamhadi <aliemamhadi@aemirage.com>                       |
+  +------------------------------------------------------------------------+
+*/
+
+/**
+ * This is part of Mirage Micro Framework
+ *
+ * @author Ali Emamhadi <aliemamhadi@gmail.com>
+ */
+
+namespace Mirage\Interfaces;
+
+/**
+ * Class Auth
+ * @package Mirage
+ */
+interface BeanstalkInterface
+{
+}
